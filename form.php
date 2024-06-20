e59d07d06b0bbc35465190d27687a5bb581cf0e7
bungkoez <stphnriscyturnip12@gmail.com>
2024-06-20 22:08:03 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+echo "<center>nama_lengkap: ".$_POST['nama_lengkap']."</center><br>";
+echo "<center>email: ".$_POST['email']."</center><br>";
+echo "<center>nomor_telepon: ".$_POST['nomor_telepon']."</center><br>";
+echo "<center>tanggal_lahir: ".$_POST['tanggal_lahir']."</center><br>";
+echo "<center>alamat_rumah: ".$_POST['alamat_rumah']."</center><br>";
+echo "<center>kota: ".$_POST['kota']."</center><br>";
+echo "<center>jenis_kelamin: ".$_POST['jenis_kelamin']."</center><br>";
+echo "<center>pilihan_ukm: ".$_POST['pilihan_ukm']."</center><br>";
+echo "<center>alamat_rumah: ".$_POST['alamat_rumah']."</center><br>";
+echo "<center>alasan_memilih: ".$_POST['alasan_memilih']."</center><br>";
+?>
+
